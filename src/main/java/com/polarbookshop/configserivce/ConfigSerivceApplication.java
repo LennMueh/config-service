41fdeaaeb7f7144defa3a80d5cad3cba3ac2c472
Lennart Mühlhahn <lennart.muehlhahn@tuhh.de>
2026-03-10 23:38:52 +0100

    Init Commit

---
+++ src/main/java/com/polarbookshop/configserivce/ConfigSerivceApplication.java
@@ -0,0 +1,15 @@
+package com.polarbookshop.configserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigSerivceApplication.class, args);
+    }
+
+}
